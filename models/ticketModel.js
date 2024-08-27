36a52cd0b52cbcb67c63f38fa01f01d69b825231
bautista.arvin.biz <bautista.arvin.biz@gmail.com>
2024-08-27 22:30:35 +0800

    Add ongoing status

--- models/ticketModel.js
+++ models/ticketModel.js
@@ -39,7 +39,7 @@ const ticketSchema = mongoose.Schema(
     status: {
       type: String,
       required: true,
-      enum: ["new", "open", "closed"],
+      enum: ["new", "open", "closed", "ongoing"],
       default: "open",
     },
     assignedTo: {
