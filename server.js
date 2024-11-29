2da62bee048b720e506afeaf4d7f2f74c3a98412
CSV Admin <denzarvinz@gmail.com>
2024-11-29 20:54:27 +0800

    Add server time API

--- server.js
+++ server.js
@@ -51,6 +51,14 @@ app.use("/api/assigns", require("./routes/assignRoutes"));
 app.use("/api/categories", require("./routes/categoryRoutes"));
 app.use("/api/employeeTimes", require("./routes/employeeTimeRoutes"));
 
+app.get("/api/current-time", (req, res) => {
+  const currentTime = new Date();
+  res.json({
+    date: currentTime.toLocaleDateString(),
+    time: currentTime.toLocaleTimeString(),
+  });
+});
+
 app.use(errorHandler);
 app.disable("x-powered-by");
 
