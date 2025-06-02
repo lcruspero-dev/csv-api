8c9a66d72b34b40ab888289d9fc789abf0eb1f0b
CSV Admin <denzarvinz@gmail.com>
2025-06-02 21:27:20 +0800

    Add TL role

--- models/userModel.js
+++ models/userModel.js
@@ -22,7 +22,7 @@ const userSchema = mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ["user", "HR", "IT", "SUPERADMIN"],
+      enum: ["user", "HR", "IT", "SUPERADMIN", "TL"],
       default: "user",
     },
     status: {
